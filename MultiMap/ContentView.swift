7452559ca4554393df07f47107ace79eee8712c8
Weerawut Chaiyasomboon <weerawut@live.com>
2024-11-22 13:27:44 +0700

    Initial Commit

---
+++ MultiMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiMap
+//
+//  Created by Weerawut Chaiyasomboon on 22/11/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Weerawut Chaiyasomboon on 22/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
